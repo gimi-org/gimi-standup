352ebac1a4f967cfea1cd11f3fff8c22afb1b608
andersback <anders.back@me.com>
2017-07-13 13:55:15 +0200

    added support for i18n

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const { exec } = require('child_process')
-var standUpCmd = 'cd .. && git-standup'
+var standUpCmd = 'cd .. && git-standup -m 2'
 var {parseStandupOutput, getOpenChromeCommandForRepo} = require('./lib')
-exec(standUpCmd, (err, stdOut) => {
-  stdOut = parseStandupOutput(stdOut)
-  Object.keys(stdOut).forEach(key => {
-    var cmd = getOpenChromeCommandForRepo(key, stdOut[key])
-    exec(cmd)
+
+exec("cd ../gimi-i18n && git fetch", (err, stdout) => {
+  exec(standUpCmd, (err, stdOut) => {
+    console.log(stdOut)
+    stdOut = parseStandupOutput(stdOut)
+    Object.keys(stdOut).forEach(key => {
+      var cmd = getOpenChromeCommandForRepo(key, stdOut[key])
+      exec(cmd)
+    })
   })
 })
