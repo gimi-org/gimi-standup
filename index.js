afa223092711030704de868766fd3b76fa3c03d3
andersback <anders.back@me.com>
2017-08-01 11:52:11 +0200

    added check for i18n path

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+var fs = require('fs')
 const { exec } = require('child_process')
-var standUpCmd = 'cd .. && git-standup -m 2'
+var standUpCmd = 'cd .. && git-standup -m 2 -f'
 var {parseStandupOutput, getOpenChromeCommandForRepo} = require('./lib')
-
-exec("cd ../gimi-i18n && git fetch", (err, stdout) => {
+var i18nPath = '../gimi-i18n'
+if(!fs.existsSync(i18nPath)) throw new Error(`Cant find: ${i18nPath}`)
+exec(`cd ${i18nPath} && git fetch`, (err, stdout) => {
   exec(standUpCmd, (err, stdOut) => {
     console.log(stdOut)
     stdOut = parseStandupOutput(stdOut)
