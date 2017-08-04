acc0058f3082eb718facf688ccb852da311be00a
andersback <anders.back@me.com>
2017-08-04 08:37:12 +0200

    gimi standup now 'works on my machine' :)

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var fs = require('fs')
 const { exec } = require('child_process')
-var standUpCmd = 'cd .. && git-standup -m 2 -f'
+var standUpCmd = 'cd .. && git-standup -m 2'
 var {parseStandupOutput, getOpenChromeCommandForRepo} = require('./lib')
 var i18nPath = '../gimi-i18n'
 if(!fs.existsSync(i18nPath)) throw new Error(`Cant find: ${i18nPath}`)
+
 exec(`cd ${i18nPath} && git fetch`, (err, stdout) => {
+  console.log(`${standUpCmd} ...`)
   exec(standUpCmd, (err, stdOut) => {
     console.log(stdOut)
     stdOut = parseStandupOutput(stdOut)
